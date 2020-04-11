068a2faa56fe0be1385ec987dc488d7a32444b1f
Sail <1782375734@qq.com>
2020-04-11 15:49:57 +0800

    date:2020/04/11

--- week2.cpp
+++ week2.cpp
@@ -98,3 +98,35 @@ public:
         return st.top().second;
     }
 };
+
+/*
+title:Diameter of Binary Tree
+description:
+	Given a binary tree, you need to compute the length of the diameter of the tree.
+	The diameter of a binary tree is the length of the longest path between any two nodes in a tree. 
+	This path may or may not pass through the root.
+example:
+	Given a binary tree
+	      1
+         / \
+        2   3
+       / \     
+      4   5  
+	Return 3,which is the length of the path [4,2,1,3] or [5,2,1,3].
+Note:The length of path between two nodes is represented by the number of edges between them. 
+*/
+int ans;
+int dfs(TreeNode* rt)
+{
+    if(rt==NULL) return 0;
+    int l=dfs(rt->left);
+    int r=dfs(rt->right);
+    ans=max(ans,l+r+1);
+    return 1+max(l,r);
+}
+int diameterOfBinaryTree(TreeNode* root) {
+    ans=0;
+    if(root==NULL) return 0;
+    dfs(root);
+    return ans-1;
+}
