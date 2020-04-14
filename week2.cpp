b87964a5cee83085642591cd648fc40a9f12546c
Sail <1782375734@qq.com>
2020-04-14 15:54:27 +0800

    date:2020/04/14

--- week2.cpp
+++ week2.cpp
@@ -200,3 +200,50 @@ int findMaxLength(vector<int>& nums)
     }
     return ans;
 }
+/*
+title: Perform String Shifts
+description:
+	You are given a string s containing lowercase English letters, and a matrix shift, where shift[i] = [direction, amount]:
+    direction can be 0 (for left shift) or 1 (for right shift). 
+    amount is the amount by which string s is to be shifted.
+    A left shift by 1 means remove the first character of s and append it to the end.
+    Similarly, a right shift by 1 means remove the last character of s and add it to the beginning.
+	Return the final string after all operations.
+Example 1:
+	Input: s = "abc", shift = [[0,1],[1,2]]
+	Output: "cab"
+	Explanation: 
+		[0,1] means shift to left by 1. "abc" -> "bca"
+		[1,2] means shift to right by 2. "bca" -> "cab"
+Example 2:
+	Input: s = "abcdefg", shift = [[1,1],[1,1],[0,2],[1,3]]
+	Output: "efgabcd"
+	Explanation:  
+		[1,1] means shift to right by 1. "abcdefg" -> "gabcdef"
+		[1,1] means shift to right by 1. "gabcdef" -> "fgabcde"
+		[0,2] means shift to left by 2. "fgabcde" -> "abcdefg"
+		[1,3] means shift to right by 3. "abcdefg" -> "efgabcd"
+Constraints:
+    1 <= s.length <= 100
+    s only contains lower case English letters.
+    1 <= shift.length <= 100
+    shift[i].length == 2
+    0 <= shift[i][0] <= 1
+    0 <= shift[i][1] <= 100
+*/
+string stringShift(string s, vector<vector<int>>& shift) 
+{
+    int cnt=0;
+    for(auto u:shift) 
+    {
+        if(u[0]==0) cnt+=u[1];
+        else cnt-=u[1];
+    }
+    const int n = s.length();
+    while(cnt<0) cnt+=n;
+    if(cnt>=n) cnt%=n;
+    //从第cnt到结尾
+    //cout<<cnt<<endl;
+    string ans=s.substr(cnt)+s.substr(0,cnt);
+    return ans;
+}
