c3fa48ac4c99431425b7e7259706f5c19264bd27
Sail <1782375734@qq.com>
2020-04-19 15:35:35 +0800

    date:2020/04/19

--- week3.cpp
+++ week3.cpp
@@ -143,3 +143,39 @@ int minPathSum(vector<vector<int>>& grid)
     }
     return dp[m-1][n-1];
 }
+/*
+title:Search in Rotated Sorted Array
+description:
+	Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+	(i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+	You are given a target value to search. If found in the array return its index, otherwise return -1.
+	You may assume no duplicate exists in the array.
+	Your algorithm's runtime complexity must be in the order of O(log n).
+Example 1:
+	Input: nums = [4,5,6,7,0,1,2], target = 0
+	Output: 4
+Example 2:
+	Input: nums = [4,5,6,7,0,1,2], target = 3
+	Output: -1
+*/
+int search(vector<int>& nums, int target) 
+{
+    const int n=nums.size();
+    int l=0,r=n-1;
+    while(l<=r)
+    {
+        int mid = (l+r)>>1;
+        if(nums[mid]==target) return mid;
+        if(nums[mid]<nums[r])
+        {
+            if(nums[mid]<target&&target<=nums[r]) l=mid+1;
+            else r=mid-1;
+        }
+        else
+        {
+            if(nums[l]<=target&&target<nums[mid]) r=mid-1;
+            else l=mid+1;
+        }
+    }
+    return -1;
+}
