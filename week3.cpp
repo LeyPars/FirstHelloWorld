19561d417f57adc7922d1208b3695f5a256f9945
Sail <1782375734@qq.com>
2020-04-20 15:57:52 +0800

    date:2020/04/20

--- week3.cpp
+++ week3.cpp
@@ -179,3 +179,40 @@ int search(vector<int>& nums, int target)
     }
     return -1;
 }
+/*
+title:Construct Binary Search Tree from Preorder Traversal
+description:
+	Return the root node of a binary search tree that matches the given preorder traversal.
+	(Recall that a binary search tree is a binary tree where for every node, any descendant of 
+	node.left has a value < node.val, and any descendant of node.right has a value > node.val. 
+	Also recall that a preorder traversal displays the value of the node first, then traverses 
+	node.left, then traverses node.right.)
+Example 1:
+	Input: [8,5,1,7,10,12]
+	Output: [8,5,10,1,7,null,12]
+Note: 
+    1 <= preorder.length <= 100
+    The values of preorder are distinct.
+*/
+TreeNode* build(vector<int>& arr,int l,int r)
+{
+    if(l>r) return NULL;
+    if(l==r) return new TreeNode(arr[l]);
+    TreeNode* cur = new TreeNode(arr[l]);
+    int edge;
+    for(int i=l+1;i<=r;++i)
+    {
+        if(arr[i]>arr[l])
+        {
+            edge=i;
+            break;
+        }
+    }
+    cur->left=build(arr,l+1,edge-1);
+    cur->right=build(arr,edge,r);
+    return cur;
+}
+TreeNode* bstFromPreorder(vector<int>& preorder) 
+{
+    return build(preorder,0,preorder.size()-1);
+}
