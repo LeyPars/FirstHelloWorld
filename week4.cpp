e8f5d6256330370bd2372cee38e40fc419fd968d
Sail <1782375734@qq.com>
2020-04-23 15:22:07 +0800

    date:2020/04/23

--- week4.cpp
+++ week4.cpp
@@ -22,3 +22,36 @@ int subarraySum(vector<int>& nums, int k) {
         }
         return ans;
     }
+	
+/*
+title:Bitwise AND of Numbers Range
+description:
+	Given a range [m, n] where 0 <= m <= n <= 2147483647, return the bitwise AND of all numbers in this range, inclusive.
+Example 1:
+	Input: [5,7]
+	Output: 4
+Example 2:
+	Input: [0,1]
+	Output: 0
+*/
+//the left most common prefix of bits
+int rangeBitwiseAnd(int m, int n) {
+        int cnt=0;
+        while(m!=n)
+        {
+            ++cnt;
+            m>>=1;
+            n>>=1;
+        }
+        return m<<cnt;
+    }
+	
+int rangeBitwiseAnd(int m,int n)
+{
+	int mask=INT_AMX;
+	while((mask&m)!=(mask&n))
+	{
+		mask<<=1;
+	}
+	return mask&m;
+}
